829e90db603363695a09101c75842679a75cae3f
ayushkhaire <codeeayush@gmail.com>
2025-03-13 23:19:04 +0530

    added code for a new format

--- stream-processing/create-stream.py
+++ stream-processing/create-stream.py
@@ -20,13 +20,23 @@ megadf = pd.DataFrame()
 
 for stock in tqdm(stocks_files[:50]): 
     df = pd.read_csv(f'../data_sourcing/source/real-time/{stock}')
+    df = df.drop_duplicates()
     megadf = pd.concat([megadf,df],axis=0)
     
+megadf = megadf.dropna()
 all_times = megadf['unix_timestamp'].unique()
 
 for a_t in tqdm(all_times):
     stock_df = megadf[megadf['unix_timestamp'] == a_t ]
-    stock_dict = stock_df.to_dict()
+    stock_dict = {
+        "unix_timestamp": stock_df["unix_timestamp"].tolist(),
+        "stockname":stock_df['stockname'].tolist(),
+        "open": stock_df["open"].tolist(),
+        "close": stock_df["close"].tolist(),
+        "high": stock_df["high"].tolist(),
+        "low": stock_df["low"].tolist(),
+        "volume": stock_df["volume"].tolist()
+    }
     stock_data = json.dumps(stock_dict)
     client.send_message(stock_data)
     time.sleep(1)
